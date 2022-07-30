9b5d426a0ce83f0f1d27109bd6a5579679c61043
RosemaryMac <rmachanja14@gmail.com>
2022-07-30 12:33:16 +0300

    getline

---
+++ getline.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ * wait - takes the address of an uninitialized integer
+ *        as an argument
+ * child - name of a process
+ */
+int main(void)
+{
+	int wait child;
+
+	else
+	{
+		wait (&wait_child);
+	}
