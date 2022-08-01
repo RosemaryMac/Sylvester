df5b00728118802d645cc3b8c30ebd1bfa69651a
RosemaryMac <rmachanja14@gmail.com>
2022-08-01 01:13:41 -0700

    ac

---
+++ ac.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - prints all arguments without using ac
+ * @args: number of arguments in av
+ * @av: array of strings (arguments)
+ *
+ * Return: Always 0.
+ */
+int main(int args, char *av[])
+{
+	int i;
+
+	for (i = 0; i < args; i++)
+		printf("%s\n", av[i]);
+
+	return (0);
+}
