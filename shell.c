c50eb8c84de550443fcfc0a625f46d79ad4dad9d
RosemaryMac <rmachanja14@gmail.com>
2022-08-08 01:33:41 -0700

    Forever Grateful

--- shell.c
+++ shell.c
@@ -10,10 +10,10 @@
 
 /**
  * main - Entry point
+ * *ptr: variable pointer
+ * args: Arguments
  *
- *
- *
- *
+ * Return: Always 0.
  */
 int main(void)
 {
@@ -27,23 +27,29 @@ int main(void)
 		printf("%s ", getuid() == 0 ? "#" : "$");
 		fgets(input, PRMTSIZ, stdin);
 
-		if (*ptr == '\n') continue;
-
-		 for (size_t i =0; i < sizeof(args) && *ptr; ptr++)
-		 {
-			 if (*ptr == ' ') continue;
-			 if (*ptr == '\n') break;
+		if (*ptr == '\n')
+			continue;
+		
+		for (size_t i =0; i < sizeof(args) && *ptr; ptr++)
+		{
+			 if (*ptr == ' ')
+				 continue;
+			 if (*ptr == '\n')
+				 break;
 			 for (args[i++] = ptr; *ptr && *ptr != ' ' && *ptr != '\n'; ptr++);
 			 *ptr = '\0';
-		 }
-		 
-		 if (strcmp(EXITCMD, args[0]) == 0) return 0;
+		}
+	
+		if (strcmp(EXITCMD, args[0]) == 0)
+			return (0);
 
-		  signal(SIGINT, SIG_DFL);
-		  if (fork() == 0) exit(execvp(args[0], args));
-		  signal(SIGINT, SIG_IGN);
+		signal(SIGINT, SIG_DFL);
+		if (fork() == 0)
+			exit(execvp(args[0], args));
+		signal(SIGINT, SIG_IGN);
 
-		   wait(&wstatus);
-		   if (WIFEXITED(wstatus)) printf("<%d>", WEXITSTATUS(wstatus));
+		wait(&wstatus);
+		if (WIFEXITED(wstatus))
+			printf("<%d>", WEXITSTATUS(wstatus));
 	}
 }
